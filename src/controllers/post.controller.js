0edd7b7afe2ed8b606f1145091332813980aa308
DrFrostyyy <jc0812011@gmail.com>
2025-09-06 14:43:36 +0800

    Challenge in Controllers

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -40,3 +40,12 @@ export const deletePost = (req, res) => {
     }
     res.status(204).send();
 };
+
+export const patchPost = (req, res) => {
+    const postId = parseInt(req.params.id, 10);
+    const post = postService.patchPost(postId, req.body);
+    if (!post) {
+        return res.status(404).json({ message: 'Post not found.' });
+    }
+    res.json(post);
+};
