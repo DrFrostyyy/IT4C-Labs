ccec3183f3f87bdf7067a297b5026c53b54acc9d
DrFrostyyy <jc0812011@gmail.com>
2025-09-13 13:16:06 +0800

    Added and condigured index.js

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-// index.js
 import express from "express";
-import dotenv from "dotenv";
+import morgan from "morgan";
+import config from "./src/config/index.js"; 
 import postRoutes from "./src/routes/post.routes.js";
-import morgan from 'morgan';
-
-dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV === "development") {
+// Environment-specific logging
+if (config.nodeEnv === "development") {
   app.use(morgan("dev"));
+} else if (config.nodeEnv === "production") {
+  app.use(morgan("combined"));  
 }
 
-app.use(morgan("dev"));
 app.use(express.json());
 
+// Routes
 app.use("/posts", postRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
 app.get("/", (req, res) => {
-  res.send(
-    "Name: Jana Cornejo, Section: IT4C, Program: Information Technology"
-  );
+  res.send("Name: Jana Cornejo, Section: IT4C, Program: Information Technology");
 });
 
 app.get("/hello/:name", (req, res) => {
-  const name = req.params.name;
-  res.send(`Hello ${name}`);
+  res.send(`Hello ${req.params.name}`);
 });
 
-app.get("/foo/", (req, res) => {
+app.get("/foo", (req, res) => {
   console.log(req.query);
+  res.send("Check console for query params");
 });
 
 app.get("/IT", (req, res) => {
   console.log(req.body);
+  res.send("Check console for body data");
+});
+
+app.listen(config.port, () => {
+  console.log(
+    `🚀 Server is running in ${config.nodeEnv} mode at http://localhost:${config.port}`
+  );
 });
