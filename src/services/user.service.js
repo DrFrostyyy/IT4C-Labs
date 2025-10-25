7c4ca9d70cfbcc387e72da3e413cd8bbe634d487
DrFrostyyy <jc0812011@gmail.com>
2025-10-25 18:19:04 +0800

    Added auth controller and routes

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,39 +3,40 @@ import pool from '../config/db.js';
 import ApiError from "../utils/ApiError.js";
 
 
-export const createUser = async (userData) => {
-    const { username, email } = userData;
-
+export const registerUser = async (userData) => {
+    const { username, email, password } = userData;
     try {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
 
         const [result] = await pool.query(
-            'INSERT INTO users (username, email) VALUES (?, ?)',
-            [username, email]
+
+            'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+
+            [username, email, hashedPassword]
         );
 
-        return await getUserById(result.insertId);
+        const newUser = await getUserById(result.insertId);
+        return newUser;
 
-    } catch (err) {
-  
-        if (err.code === 'ER_DUP_ENTRY') {
-            throw new ApiError(409, 'Username or email already exists.');
+    } catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') {
+            throw new ApiError(409, "Username or email already exists.");
         }
-        // Other errors
-        throw new ApiError(500, 'Failed to create user.');
+        throw error;
     }
 };
 
+
 export const getUserById = async (id) => {
-    const [rows] = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
-
+    const [rows] = await pool.query('SELECT id, username, email, createdAt FROM users WHERE id = ?', [id]);
     if (rows.length === 0) {
-        throw new ApiError(404, 'User not found.');
+        throw new ApiError(404, "User not found");
     }
-
     return rows[0];
 };
 
 export const getAllUsers = async () => {
-    const [rows] = await pool.query('SELECT * FROM users');
-    return rows;
+    const [users] = await pool.query('SELECT id, username, email, createdAt FROM users');
+    return users;
 };
