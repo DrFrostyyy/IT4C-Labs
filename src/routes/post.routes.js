29ac5da9d91c777cce36556c99417ba78c12146a
DrFrostyyy <jc0812011@gmail.com>
2025-10-25 19:26:29 +0800

    Creating and Applying the Authentication Middleware

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { validatePost } from '../middlewares/validator.middleware.js';
 import * as postController from '../controllers/post.controller.js';
+import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
@@ -12,4 +13,5 @@ router.delete('/:id', postController.deletePost);
 router.post('/', validatePost, postController.createPost);
 router.put('/:id', validatePost, postController.updatePost);
 router.patch('/:id', postController.patchPost);
+router.post('/', authMiddleware, validatePost, postController.createPost);
 export default router;
